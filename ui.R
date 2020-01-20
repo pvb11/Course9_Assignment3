43ba14e13c8d357d46518e5e0acbb59e1794dd0c
pvb11 <40701656+pvb11@users.noreply.github.com>
2020-01-20 18:10:40 -0500

    Add files via upload

--- ui.R
+++ ui.R
@@ -8,6 +8,10 @@ pageWithSidebar(
                  min = 1, max = 8)
   ),
   mainPanel(
-    plotOutput('plot1')
+    plotOutput('plot1'),
+    'DOCUMENTATION:',
+    'All of the R code used for this project is found in the git repository https://github.com/pvb11/Course9_Assignment3 in the server.R and ui.R files.',
+    'This application deploys k-means clustering on the rock dataset. The user can select which variables to compare, as well as the number of clusters to display.',
+    'The rock dataset consists of 48 rock sample measurements from a petroleum reservoir. Twelve core samples from petroleum reservoirs were sampled by 4 cross-sections. Each core sample was measured for permeability, and each cross-section has total area of pores, total perimeter of pores, and shape.'
   )
 )
